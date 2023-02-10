f3dd5c791ad000062d74d9ef1b9b3eb6c3ce501c
eSSeGH <ste.virgadaula@gmail.com>
2023-02-10 17:02:57 +0100

    JS bombGen function added

--- js/app.js
+++ js/app.js
@@ -13,12 +13,17 @@ btnPlay.addEventListener("click", startGame)
 // crea un array da 1 a sidegrid, con numeri random, non doppi
 
 function bombGen(bombNum) {
+    console.log("bombGenFunc toggled")
 
     let numArray = []
+    console.log("numArray è stato creato")
 
-    while (numArray < bombNum.length) {
+    while (numArray.length < bombNum) {
 
-        let num = randMinMax(1, bombNum)
+        console.log("ciclo while partito")
+
+        let range = bombNum**2
+        let num = randMinMax(1, range)
         console.log("il numero ciclato è", num)
         console.log("l'array di numeri è", numArray)
 
@@ -70,7 +75,10 @@ function startGame() {
 
     createGrid(sideGrid)
 
-    bombGen(sideGrid)
+    let bombNum = parseInt(sideGrid)
+
+    console.log("la variabile di bombGen:", bombNum)
+    bombGen(bombNum)
 
     btnPlay.innerHTML = "RESTART"
 }
