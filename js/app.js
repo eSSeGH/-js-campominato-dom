2c00289c6efc362548610268423707a6c4a4a983
eSSeGH <ste.virgadaula@gmail.com>
2023-02-12 15:55:09 +0100

    DEBUG gameover  listeners, EDIT+JS highscore added

--- js/app.js
+++ js/app.js
@@ -8,10 +8,10 @@ let bombArray = []
 
 let scoreEl = document.querySelector(".score")
 let score = 0
+let highScoreEl = document.querySelector(".high-score")
+let highScore = 0
 
 btnPlay.addEventListener("click", startGame)
-
-
 
 // FUNZIONI
 
@@ -70,6 +70,30 @@ function startGame() {
         }
     }
 
+    const controller = new AbortController
+
+    function createGrid(sideOfGrid) {
+
+        let cellNum = sideOfGrid**2
+    
+        for (i=0; i < cellNum; i++) {
+    
+            num = i + 1
+    
+            const cellEl = document.createElement("div")
+    
+            cellEl.className = "cell"
+            cellEl.style.width = `calc(100%/ ${sideOfGrid})`
+            cellEl.style.height = `calc(100%/ ${sideOfGrid})`
+            cellEl.innerHTML = num
+    
+            gridEl.append(cellEl)
+    
+            cellEl.addEventListener("click", onClick, {signal: controller.signal})
+        }
+    
+    }
+
     createGrid(sideGrid)
 
     let bombNum = parseInt(sideGrid)
@@ -78,58 +102,60 @@ function startGame() {
     console.log(bombArray)
 
     btnPlay.innerHTML = "RESTART"
+
+    function onClick() {
+
+        console.log(bombArray)
+    
+        const clickedCell = this
+        console.dir(clickedCell)
+    
+        num = parseInt(clickedCell.innerHTML)
+        console.log(num)
+    
+        if (bombArray.includes(num)) {
+            console.log("bombArray include num")
+            clickedCell.style.backgroundColor = "red"
+            clickedCell.innerHTML = "<i class='fa-solid fa-bomb'></i>"
+            scoreEl.style.backgroundColor = "red"
+            scoreEl.innerHTML = `YOU LOSE: SCORE(${score})`
+
+            console.log(score)
+            console.log(highScore)
+
+            if (score > highScore) {
+                highScore = parseInt(score)
+                highScoreEl.innerHTML = `HS: ${highScore}`
+            }
+
+            controller.abort()
+    
+        } else {
+            console.log("bombArray non include num")
+            clickedCell.style.backgroundColor = "lightblue"
+            score += 1
+            scoreEl.innerHTML = score
+        }
+    
+        clickedCell.removeEventListener("click", onClick)
+    }
 }
 
 // CREATE GRID FUNCTION
-function createGrid(sideOfGrid) {
 
-    let cellNum = sideOfGrid**2
-
-    for (i=0; i < cellNum; i++) {
-
-        num = i + 1
-
-        const cellEl = document.createElement("div")
-        console.dir
-
-        cellEl.className = "cell"
-        cellEl.style.width = `calc(100%/ ${sideOfGrid})`
-        cellEl.style.height = `calc(100%/ ${sideOfGrid})`
-        cellEl.innerHTML = num
-
-        gridEl.append(cellEl)
-
-        cellEl.addEventListener("click", onClick)
-    }
-}
 
 // CLICK CELL FUNCTION
-function onClick() {
 
-    console.log(bombArray)
 
-    const clickedCell = this
-    console.dir(clickedCell)
+// REMVE ALL EVENT LISTENER FROM CELLS
 
-    num = parseInt(clickedCell.innerHTML)
-    console.log(num)
+// function removeAllListener() {
 
-    if (bombArray.includes(num)) {
-        console.log("bombArray include num")
-        clickedCell.style.backgroundColor = "red"
-        clickedCell.innerHTML = "<i class='fa-solid fa-bomb'></i>"
-        scoreEl.style.backgroundColor = "red"
-        scoreEl.innerHTML = "YOU LOSE"
-    } else {
-        console.log("bombArray non include num")
-        clickedCell.style.backgroundColor = "lightblue"
-        score += 1
-        scoreEl.innerHTML = score
-    }
+//     let cellAllEl = document.querySelectorAll(".cell")
+//     console.log(cellAllEl)
 
-    clickedCell.removeEventListener("click", onClick)
-}
-
+//     cellAllEl.removeEventListener("click", onClick)
+// }
 
 
 
